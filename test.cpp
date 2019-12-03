79a0174de17e275a10667a62c0c4573d43bbc868
YazongMa <mayazong@126.com>
2019-12-03 16:38:14 +0800

    ad

--- test.cpp
+++ test.cpp
@@ -2,59 +2,16 @@
 #include <string.h>
 #include <stdio.h>
 
-using namespace std;
+#include "Chat2Network.h"
+#include "UserAction.h"
 
-
-#define	d_LAB_STR_SIZE	16
-
-
-class CUserObject
-{
-public:
-	static void OutToUser(const unsigned int nState, void* pData, unsigned int nSize);
-	static void WaitUserInput(const unsigned int nState, void* pData, unsigned int nSize);
-};
-
+extern int TestNullPtr();
+extern void TestUserAction();
 
 int main()
 {
-	int n = 5;
-	char (*pAry)[d_LAB_STR_SIZE] = new char[n][d_LAB_STR_SIZE];
-
-	memset(pAry, 0, n * d_LAB_STR_SIZE);
-	for (int i=0; i<n; ++i)
-	{
-		sprintf(pAry[i], "%04d%04d%04d%03d", i, i, i, i);
-		cout << "current data:" << pAry[i] << endl;
-	}
-	cout << "--------------------------------" << endl;
-	cout << endl;
-
-	CUserObject::OutToUser(1, pAry, n * d_LAB_STR_SIZE);
-
+	TestNullPtr();
+	Chat2Baidu();
 	return 0;
 }
 
-
-void
-CUserObject::OutToUser(const unsigned int nState, void* pData, unsigned int nSize)
-{
-	int n = nSize / d_LAB_STR_SIZE;
-	cout << "size:"<< n << endl;
-
-	char *pszData = (char *)pData;
-	char szText[d_LAB_STR_SIZE] = { 0 };
-	for (int i=0; i<n; ++i)
-	{
-		memset(szText, 0, sizeof(szText));
-		memcpy(szText, pszData + i*d_LAB_STR_SIZE, d_LAB_STR_SIZE);
-
-		cout << "OutToUser data:" << szText << endl;
-	}
-}
-
-
-void
-CUserObject::WaitUserInput(const unsigned int nState, void* pData, unsigned int nSize)
-{
-}
